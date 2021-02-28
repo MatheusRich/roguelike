af586ca1a42aa168504c94ac69b3a7413839245c
Matheus Richard <matheusrichardt@gmail.com>
2021-02-27 23:02:02 -0300

    Let there be code!

---
+++ lib/roguelike.rb
@@ -0,0 +1,6 @@
+require "roguelike/version"
+
+module Roguelike
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/roguelike_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Roguelike do
+  it "has a version number" do
+    expect(Roguelike::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
