728c35da18aef3a0938c894c148f70d07af15665
Matheus Richard <matheusrichardt@gmail.com>
2021-03-03 18:11:37 -0300

    Add GameMap

---
+++ lib/roguelike/game_map.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require_relative "tiles"
+
+module Roguelike
+  using RichEngine::StringColors
+
+  class GameMap
+    attr_reader :tiles
+
+    def initialize(width:, height:)
+      @width = width
+      @height = height
+      @tiles = Array.new(width) { Array.new(height) { Tiles::Floor } }
+      @tiles[22].fill(Tiles::Wall, 15..20)
+    end
+
+    def in_bounds?(x:, y:)
+      x_in_bounds = (0...@width).cover? x
+      y_in_bounds = (0...@height).cover? y
+
+      x_in_bounds && y_in_bounds
+    end
+
+    def out_of_bounds?(x:, y:)
+      !in_bounds?(x: x, y: y)
+    end
+
+    def render(canvas:)
+      @tiles.each_with_index do |row, i|
+        row.each_with_index do |tile, j|
+          canvas[i, j] = tile.dark.to_s
+        end
+      end
+    end
+  end
+end
