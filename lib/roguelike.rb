c30b566b183559fba19b1cb9d850d1c5f7a4456f
Matheus Richard <matheusrichardt@gmail.com>
2021-03-03 18:11:51 -0300

    Use gamemap

--- lib/roguelike.rb
+++ lib/roguelike.rb
@@ -5,22 +5,31 @@ require "roguelike/version"
 require "roguelike/engine"
 require "roguelike/entity"
 require "roguelike/event_handler"
+require "roguelike/game_map"
 
 module Roguelike
   class Exit < StandardError; end
 
   class Game < RichEngine::Game
-    FPS = 30
+    FPS = 30.0
 
     def on_create
+      system("tput civis") # hide cursor
+      @stty_orig = `stty -g` # save previous state
+      system("stty -echo") # disable echo
+
       @canvas = RichEngine::Canvas.new(@width, @height, bg: ".")
       @game_over = false
+
+      @map_width = 80
+      @map_height = 25
 
       @player = Entity.new(x: @width / 2, y: @height / 2, char: "@", color: :white)
       @npc = Entity.new(x: @width / 2 - 5, y: @height / 2, char: "@", color: :red)
 
       @event_handler = EventHandler.new
-      @engine = Engine.new(entities: [@player, @npc], event_handler: @event_handler, player: @player)
+      @game_map = GameMap.new(width: @map_width, height: @map_height)
+      @engine = Engine.new(entities: [@player, @npc], event_handler: @event_handler, player: @player, game_map: @game_map)
     end
 
     def on_update(_dt, key)
@@ -30,6 +39,11 @@ module Roguelike
       sleep 1 / FPS
 
       keep_playing?
+    end
+
+    def on_destroy
+      system("tput cnorm") # display cusor
+      system("stty #{@stty_orig}") # restore echo
     end
 
     private
--- lib/roguelike/engine.rb
+++ lib/roguelike/engine.rb
@@ -7,17 +7,20 @@ module Roguelike
   using RichEngine::StringColors
 
   class Engine
-    def initialize(entities:, event_handler:, player:)
+    def initialize(entities:, event_handler:, player:, game_map:)
       @entities = entities.to_set
       @event_handler = event_handler
       @player = player
+      @game_map = game_map
     end
 
     def handle_events(key)
       action = @event_handler.ev_keydown(key)
 
       if action.is_a? MovementAction
-        @player.move(dx: action.dx, dy: action.dy)
+        if walkable_tile?(x: @player.x + action.dx, y: @player.y + action.dy)
+          @player.move(dx: action.dx, dy: action.dy)
+        end
       elsif action.is_a? EscapeAction
         raise Roguelike::Exit
       end
@@ -26,11 +29,19 @@ module Roguelike
     def render(canvas, io)
       canvas.clear
 
+      @game_map.render(canvas: canvas)
+
       @entities.each do |entity|
-        canvas[entity.x, entity.y] = entity.char.send(entity.color)
+        canvas[entity.x, entity.y] = entity.char.send(entity.color).on_black
       end
 
       io.write(canvas.canvas)
+    end
+
+    private
+
+    def walkable_tile?(x:, y:)
+      @game_map.tiles[x][y].walkable
     end
   end
 end
