d6c9b161582470c21b3b2187e1504a5c5d01d067
Matheus Richard <matheusrichardt@gmail.com>
2021-03-08 18:16:57 -0300

    Add new tiles

--- lib/roguelike/tiles.rb
+++ lib/roguelike/tiles.rb
@@ -4,9 +4,11 @@ module Roguelike
   using RichEngine::StringColors
 
   class Graphic
-    attr_reader :to_s
+    attr_reader :to_s, :bg, :fg
 
     def initialize(char:, bg:, fg:)
+      @bg = bg
+      @fg = fg
       @to_s = char.fg(fg).bg(bg)
     end
   end
@@ -23,23 +25,41 @@ module Roguelike
   end
 
   module Tiles
+    Grass = Tile.new(
+      walkable:    true,
+      transparent: true,
+      dark:        Graphic.new(char: "█", bg: :transparent, fg: :green),
+      light:       Graphic.new(char: "█", bg: :transparent, fg: :bright_green)
+    )
+    Water = Tile.new(
+      walkable:    false,
+      transparent: true,
+      dark:        Graphic.new(char: "█", bg: :transparent, fg: :blue),
+      light:       Graphic.new(char: "█", bg: :transparent, fg: :bright_blue)
+    )
+    Lava = Tile.new(
+      walkable:    false,
+      transparent: true,
+      dark:        Graphic.new(char: "█", bg: :transparent, fg: :red),
+      light:       Graphic.new(char: "█", bg: :transparent, fg: :bright_red)
+    )
     Floor = Tile.new(
       walkable:    true,
       transparent: true,
-      dark:        Graphic.new(char: "█", bg: :transparent, fg: :blue),
+      dark:        Graphic.new(char: "█", bg: :transparent, fg: :black),
       light:       Graphic.new(char: "█", bg: :transparent, fg: :yellow)
     )
     Wall = Tile.new(
       walkable:    false,
       transparent: false,
-      dark:        Graphic.new(char: "█", bg: :transparent, fg: :bright_blue),
+      dark:        Graphic.new(char: "█", bg: :transparent, fg: :bright_black),
       light:       Graphic.new(char: "█", bg: :transparent, fg: :bright_yellow)
     )
     Shroud = Tile.new(
       walkable:    false,
       transparent: false,
-      dark:        Graphic.new(char: "█", bg: :transparent, fg: :black),
-      light:       Graphic.new(char: "█", bg: :transparent, fg: :white)
+      dark:        Graphic.new(char: "█", bg: :transparent, fg: :transparent),
+      light:       Graphic.new(char: "█", bg: :transparent, fg: :transparent)
     )
   end
 end
