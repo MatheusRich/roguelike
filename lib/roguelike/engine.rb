8c4dcf2db538e8af854ec1a42e87c617b72be4eb
Matheus Richard <matheusrichardt@gmail.com>
2021-03-07 23:49:47 -0300

    Add basic FOV

--- lib/roguelike/engine.rb
+++ lib/roguelike/engine.rb
@@ -7,6 +7,8 @@ module Roguelike
   using RichEngine::StringColors
 
   class Engine
+    PLAYER_FOV_RADIUS = 2.5
+
     attr_reader :game_map
 
     def initialize(entities:, event_handler:, player:, game_map:)
@@ -14,12 +16,17 @@ module Roguelike
       @event_handler = event_handler
       @player = player
       @game_map = game_map
+
+      update_fov
     end
 
     def handle_events(key)
       action = @event_handler.ev_keydown(key)
 
+      return if action.nil?
+
       action.call(engine: self, entity: @player)
+      update_fov
     end
 
     def render(canvas, io)
@@ -28,10 +35,47 @@ module Roguelike
       @game_map.render(canvas: canvas)
 
       @entities.each do |entity|
-        canvas[entity.x, entity.y] = entity.char.send(entity.color).on_black
+        canvas[entity.x, entity.y] = entity.char.send(entity.color).on_blue
       end
 
       io.write(canvas.canvas)
+    end
+
+    private
+
+    def update_fov
+      @game_map.visible.vec = compute_fov(
+        transparent_tiles: @game_map.visible.vec,
+        pov:               @player.coords,
+        radius:            PLAYER_FOV_RADIUS
+      )
+
+      merge_explored_tiles_with_visible_tiles
+    end
+
+    def compute_fov(transparent_tiles:, pov:, radius:)
+      pov_x, pov_y = pov
+      start_x = [pov_x - radius, 0].max
+      start_y = [pov_y - radius, 0].max
+
+      end_x = [pov_x + radius, transparent_tiles.size].min
+      end_y = [pov_y + radius, transparent_tiles.size].min
+
+      transparent_tiles.map.with_index do |line, i|
+        line.map.with_index do |value, j|
+          if i.between?(start_x, end_x) && j.between?(start_y, end_y)
+            distance_to_pov = Calc.distance_between_points(x1: i, y1: j, x2: pov_x, y2: pov_y)
+
+            distance_to_pov <= radius
+          else
+            value
+          end
+        end
+      end
+    end
+
+    def merge_explored_tiles_with_visible_tiles
+      @game_map.explored.vec = @game_map.explored.zip(@game_map.visible).map(&:any?)
     end
   end
 end
--- lib/roguelike/game_map.rb
+++ lib/roguelike/game_map.rb
@@ -7,11 +7,14 @@ module Roguelike
 
   class GameMap
     attr_reader :tiles, :width, :height
+    attr_accessor :visible, :explored
 
     def initialize(width:, height:)
       @width = width
       @height = height
       @tiles = RichEngine::Vec2.new(width: width, height: height, fill_with: Tiles::Wall)
+      @visible = RichEngine::Vec2.new(width: width, height: height, fill_with: false)
+      @explored = RichEngine::Vec2.new(width: width, height: height, fill_with: false)
     end
 
     def in_bounds?(x:, y:)
@@ -31,8 +34,28 @@ module Roguelike
 
     def render(canvas:)
       @tiles.each_with_indexes do |tile, i, j|
-        canvas[i, j] = tile.dark.to_s
+        canvas[i, j] = if visible?(i, j)
+                         tile.light.to_s
+                       elsif explored?(i, j)
+                         tile.dark.to_s
+                       else
+                         Tiles::Shroud.dark.to_s
+                       end
       end
+    end
+
+    def transparent_tiles
+      @tiles.map(&:transparent)
+    end
+
+    private
+
+    def visible?(x, y)
+      @visible[x, y]
+    end
+
+    def explored?(x, y)
+      @explored[x, y]
     end
   end
 end
